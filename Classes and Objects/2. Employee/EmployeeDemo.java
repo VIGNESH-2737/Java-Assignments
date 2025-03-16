07fcfebf79cd240fe21f5c6713b9f6d1d26936ee
VIGNESH-2737 <89506425+VIGNESH-2737@users.noreply.github.com>
2025-03-16 15:22:12 +0530

    Create EmployeeDemo.java

---
+++ Classes and Objects/2. Employee/EmployeeDemo.java
@@ -0,0 +1,11 @@
+package com.vignesh.zohotask;
+
+public class EmployeeDemo {
+	
+	public static void main(String[] args) {
+	Employee Vignesh = new Employee(1, "Vignesh", "Dhayalan", "dhayalvignesh@gmail.com", "6382740083",
+			"ZSGS", "Trainee", 0, "6th-Feb-2025", "Active");
+	
+	System.out.println(Vignesh.toString());	 
+}
+}
